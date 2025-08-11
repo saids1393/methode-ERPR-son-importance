7688829b0fab1aa7be878c64c73063084100b85d
saids9113 <soidroudinesaid51@gmail.com>
2025-08-11 03:20:57 +0200

    Corrections du fichier next.config pour éviter l'ecran noir 7

--- app/merci/page.tsx
+++ app/merci/page.tsx
@@ -3,7 +3,7 @@ import MerciClient from '@/app/components/merci/MerciClient';
 
 export default function MerciPage() {
   return (
-    <Suspense fallback={<div>Chargement...</div>}>
+    <Suspense fallback={<div>Chargement....</div>}>
       <MerciClient />
     </Suspense>
   );
